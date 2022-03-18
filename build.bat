9b89caaf7acb25d8a0e7ea547be25d1eafa0763d
anunna <anunna@lilitusec.net>
2022-03-18 18:54:10 +0200

    restructuring

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 
-mkdir ..\..\build
-pushd ..\..\build
-cl -Zi ..\handmade\win32_handmade.cpp user32.lib gdi32.lib
+mkdir build
+pushd build
+cl -Zi win32_handmade.cpp user32.lib gdi32.lib
 popd
